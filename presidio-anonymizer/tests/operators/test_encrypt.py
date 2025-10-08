af11e319ba1b23ba938460a9f321f0cbe72290c5
Chase Kowalsky <231607559+Lunar8182@users.noreply.github.com>
2025-10-08 18:47:10 +0000

    fixing the lab7 branch

--- presidio-anonymizer/tests/operators/test_encrypt.py
+++ presidio-anonymizer/tests/operators/test_encrypt.py
@@ -38,16 +38,12 @@ def test_given_verifying_an_valid_length_key_no_exceptions_raised():
 def test_given_verifying_an_valid_length_bytes_key_no_exceptions_raised():
     Encrypt().validate(params={"key": b'1111111111111111'})
 
-@mock.patch("presidio_anonymizer.operators.aes_cipher.AESCipher.is_valid_key_size", return_value=False)
-def test_given_verifying_an_invalid_length_key_then_ipe_raised(mock_is_valid_key_size):
-    with pytest.raises(
-        InvalidParamError,
-        match="Invalid input, key must be of length 128, 192 or 256 bits",
-    ):
-        Encrypt().validate(params={"key": "key"})
 
-@mock.patch("presidio_anonymizer.operators.aes_cipher.AESCipher.is_valid_key_size", return_value=False)
+@mock.patch("presidio_anonymizer.operators.aes_cipher.AESCipher.is_valid_key_size")
 def test_given_verifying_an_invalid_length_bytes_key_then_ipe_raised(mock_is_valid_key_size):
+
+    mock_is_valid_key_size.return_value = False
+
     with pytest.raises(
         InvalidParamError,
         match="Invalid input, key must be of length 128, 192 or 256 bits",
@@ -60,19 +56,15 @@ def test_operator_name():
     assert Encrypt().operator_name() == "encrypt"
 
 def test_operator_type():
-    from presidio_anonymizer.operators.encrypt import Encrypt
-    from presidio_anonymizer.operators.operator import OperatorType
-
-    assert Encrypt().operator_type() == OperatorType.Anonymize.types[0]
+    assert Encrypt().operator_type() == OperatorType.Anonymize
 
 @pytest.mark.parametrize("key", [
-    "A" * 16,  # 128 bits (16 bytes)
-    "B" * 24,  # 192 bits (24 bytes)
-    "C" * 32,  # 256 bits (32 bytes)
-    b"D" * 16, # 128 bits (bytes)
-    b"E" * 24, # 192 bits (bytes)
-    b"F" * 32, # 256 bits (bytes)
+    "a" * 16,  
+    "b" * 24,  
+    "c" * 32, 
+    b"d" * 16, 
+    b"e" * 24, 
+    b"f" * 32, 
 ])
 def test_valid_keys(key):
-    encryptor = Encrypt()
-    encryptor.validate(params={"key": key})
+    Encrypt().validate(params={"key": key})
