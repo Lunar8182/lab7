4fb9dde7790be25f4cf83905728f890f5ac1639a
Chase Kowalsky <231607559+Lunar8182@users.noreply.github.com>
2025-10-08 18:58:03 +0000

    #

--- presidio-anonymizer/tests/operators/test_encrypt.py
+++ presidio-anonymizer/tests/operators/test_encrypt.py
@@ -2,7 +2,7 @@ from unittest import mock
 
 import pytest
 
-from presidio_anonymizer.operators import Encrypt, AESCipher
+from presidio_anonymizer.operators import Encrypt, AESCipher, OperatorType
 from presidio_anonymizer.entities import InvalidParamError
 
 #t
@@ -24,6 +24,9 @@ def test_given_anonymize_with_bytes_key_then_aes_encrypt_result_is_returned(
 ):
     expected_anonymized_text = "encrypted_text"
     mock_encrypt.return_value = expected_anonymized_text
+    encryptor = Encrypt()
+    encryptor.validate(params={"key": "key"})  
+
 
     anonymized_text = Encrypt().operate(text="text",
                                         params={"key": b'1111111111111111'})
@@ -37,6 +40,7 @@ def test_given_verifying_an_valid_length_key_no_exceptions_raised():
 
 def test_given_verifying_an_valid_length_bytes_key_no_exceptions_raised():
     Encrypt().validate(params={"key": b'1111111111111111'})
+
 
 
 @mock.patch("presidio_anonymizer.operators.aes_cipher.AESCipher.is_valid_key_size")
@@ -62,9 +66,9 @@ def test_operator_type():
     "a" * 16,  
     "b" * 24,  
     "c" * 32, 
-    b"d" * 16, 
-    b"e" * 24, 
-    b"f" * 32, 
+    "d" * 16, 
+    "e" * 24, 
+    "f" * 32, 
 ])
 def test_valid_keys(key):
     Encrypt().validate(params={"key": key})
